7bb17ab2ff9e1d9d38e274e74ecd528a3e6ebd36
sajjadnayem <18103052@iubat.edu>
2021-11-26 22:08:51 +0600

    template integration done

---
+++ resources/views/admin/fixed/sidebar.blade.php
@@ -0,0 +1,106 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <div class="user-profile">
+      <div class="user-image">
+        <img src="{{url('Backend/images/faces/face29.png')}}">
+      </div>
+      <div class="user-name">
+          Syed Alvi Islam Nifaz
+      </div>
+      <div class="user-designation">
+          Site Admin
+      </div>
+    </div>
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="icon-box menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="icon-head menu-icon"></i>
+          <span class="menu-title">Admin</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="{{url('/crisis')}}">Crisis</a></li>
+            <li class="nav-item"> <a class="nav-link" href="">Report</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="icon-head menu-icon"></i>
+          <span class="menu-title">Donor</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Donor Profile</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Donation</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+          <i class="icon-head menu-icon"></i>
+          <span class="menu-title">Volunteer</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="ui-basic">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Volunteer Profile</a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/ui-features/typography.html">Distribution</a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/forms/basic_elements.html">
+          <i class="icon-file menu-icon"></i>
+          <span class="menu-title">Form elements</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/charts/chartjs.html">
+          <i class="icon-pie-graph menu-icon"></i>
+          <span class="menu-title">Charts</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/tables/basic-table.html">
+          <i class="icon-command menu-icon"></i>
+          <span class="menu-title">Tables</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pages/icons/feather-icons.html">
+          <i class="icon-help menu-icon"></i>
+          <span class="menu-title">Icons</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+          <i class="icon-head menu-icon"></i>
+          <span class="menu-title">User Pages</span>
+          <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="auth">
+          <ul class="nav flex-column sub-menu">
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
+            <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="docs/documentation.html">
+          <i class="icon-book menu-icon"></i>
+          <span class="menu-title">Documentation</span>
+        </a>
+      </li>
+    </ul>
+  </nav
